2fe1eb8e35534914f159d065235a9f1d53844ac7
Camilo Gutierrez <gcemas20@gmail.com>
2020-06-02 23:42:37 -0500

    usercard component

--- social-taws/src/components/UserCard/UserCard.js
+++ social-taws/src/components/UserCard/UserCard.js
@@ -10,8 +10,24 @@ class UserCard extends Component{
   render() {
     return(
       <>
-        <div className="container">
-          <p>My name is: {this.props.username}</p>
+        <div className="bg-light shadow-sm rounded text-center p-3 mb-4">
+          <div className="profile-thumb mt-3 mb-4">
+            <img className="rounded-circle" src="images/profile-thumb.jpg" alt=""/>
+          </div>
+          <p className="text-3 font-weight-500 mb-2">Camilo Gutierrez</p>
+          <a>@cami3__</a>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <p>Friends</p>
+                <p>30</p>
+              </div>
+              <div className="col-6">
+                <p>Posts</p>
+                <p>3</p>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
